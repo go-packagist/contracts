26206c8ad9e80d8370711db8cb28fe73486bb8bf
Flc゛ <four_leaf_clover@foxmail.com>
2023-04-08 14:56:33 +0800

    tests: Added Test Privider

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -1,13 +1,16 @@
 package provider
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 type testProvider struct {
 	*UnimplementedProvider
 }
 
 func TestPrivider(t *testing.T) {
-	if _, ok := interface{}(&testProvider{}).(Provider); !ok {
-		t.Error("testProvider does not implement Provider")
-	}
+	_, ok := interface{}(&testProvider{}).(Provider)
+
+	assert.True(t, ok)
 }
