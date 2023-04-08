883c48f63ca50ef1df954f9ab0693d81d08c6e50
Flc゛ <four_leaf_clover@foxmail.com>
2023-04-08 17:35:01 +0800

    docs: Added `Provider` docs

--- provider/provider.go
+++ provider/provider.go
@@ -1,11 +1,28 @@
 package provider
 
 type Provider interface {
+	// Register is called when the application is registering the provider.
 	Register()
+
+	// Boot is called when the application is booting the provider.
 	Boot()
 }
 
 // UnimplementedProvider is a default implementation of the Provider interface.
+// It is used to avoid implementing all methods of the Provider interface.
+//
+// Example:
+// package provider
+//
+//	type MyProvider struct {
+//		*provider.UnimplementedProvider
+//	}
+//
+//	var _ provider.Provider = (*MyProvider)(nil)
+//
+//	func (p *MyProvider) Register() {
+//		// do something
+//	}
 type UnimplementedProvider struct {
 }
 
